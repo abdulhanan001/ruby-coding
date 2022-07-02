fb10f2d8e37ebf553bd34028ab4e05cead8bd38d
Abdulhanan <abdulhanan347@gmail.com>
2022-07-02 16:18:56 +0500

    CODING TEST::Added Serializer functionality

--- app/serializer.rb
+++ app/serializer.rb
@@ -1,2 +1,28 @@
 class Serializer
+  @@attrs = []
+
+  def initialize(object)
+    @@object = object
+  end
+
+  def serialize
+    result = {}
+
+    @@attrs.each do |attr|
+      result[attr] = @@object.send(attr) if attr.is_a? Symbol
+      result[attr[:key]] = attr[:block].call if attr.is_a? Hash
+    end
+
+    result
+  end
+
+  def self.object
+    @@object
+  end
+
+  def self.attribute(attr, &block)
+    return @@attrs << attr unless block_given?
+
+    @@attrs << { key: attr, block: block }
+  end
 end
